d38a58b318d0c1287f1b3c4d2a99e70318c0affa
mayifan02 <mayifan02@B000000111239K.local>
2020-11-10 17:01:33 +0800

    123

--- publish.sh
+++ publish.sh
@@ -2,13 +2,15 @@
 # npm config set registry http://registry.npmjs.org
 # npm version patch
 # npm publish
-
-
-lastversion=`npm view test-publish-npm-myf version`
-nowversion=`npm version patch`
+# 拿到之前版本号
+last_version=`npm view test-publish-npm-myf version`
+# 构造一个新的版本号
+now_version=`npm version patch`
 echo $lastversion
 echo $nowversion
+
+if [ $lastversion > $nowversion ];then
+    echo "不成立"
+    exit 1
+fi
 exit 0
-# if [ "1.0.8" > "1.0.9" ];then
-# echo "[ > ]"
-# fi
