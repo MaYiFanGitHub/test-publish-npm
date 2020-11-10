d409c1e2b243b88f2207fec14b0bf5bac5864a4d
mayifan02 <mayifan02@B000000111239K.local>
2020-11-10 16:58:59 +0800

    123

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ lastversion=`npm view test-publish-npm-myf version`
 nowversion=`npm version patch`
 echo $lastversion
 echo $nowversion
-
+exit 0
 # if [ "1.0.8" > "1.0.9" ];then
 # echo "[ > ]"
 # fi
