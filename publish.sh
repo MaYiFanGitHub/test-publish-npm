ce56c14d72ec08d3659d9ec6fa0f4b60178d6273
mayifan02 <mayifan02@B000000111239K.local>
2020-11-10 17:04:29 +0800

    1

--- publish.sh
+++ publish.sh
@@ -6,10 +6,10 @@
 last_version=`npm view test-publish-npm-myf version`
 # 构造一个新的版本号
 now_version=`npm version patch`
-echo $lastversion
+echo "^$lastversion"
 echo $nowversion
 
-if [ $lastversion > $nowversion ];then
+if [ "^$lastversion" > $nowversion ];then
     echo "不成立"
     exit 1
 fi
