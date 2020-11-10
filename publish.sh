3a31966bac5af8d65beeca0c5741f025ae2d9769
mayifan02 <mayifan02@B000000111239K.local>
2020-11-10 16:57:01 +0800

    test

--- publish.sh
+++ publish.sh
@@ -1,7 +1,14 @@
 # git add ./
 # npm config set registry http://registry.npmjs.org
-npm version patch
+# npm version patch
 # npm publish
-echo $?
-echo "----发布成功----"
-exit 1
+
+
+lastversion=`npm view test-publish-npm-myf version`
+nowversion=`npm version patch`
+echo $lastversion
+echo $nowversion
+
+# if [ "1.0.8" > "1.0.9" ];then
+# echo "[ > ]"
+# fi
