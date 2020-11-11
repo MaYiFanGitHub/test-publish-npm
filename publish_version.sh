c4b33648b082d6ec7d26e2000dfb9ab4e418b248
mayifan02 <mayifan02@B000000111239K.local>
2020-11-11 19:04:45 +0800

    123

--- publish_version.sh
+++ publish_version.sh
@@ -26,14 +26,14 @@ function build_version() {
 
 # 发包
 function publish() {
-    version=${$1#*//}
+    version=$1
     print "----正在发布版本 $version...----" "[32m"
     # npm publish --registry=http://registry.npm.baidu-int.com
     npm publish
     if [ $? -eq 0 ]; then
         now_version=`npm view test-publish-npm-myf version`
-        print "----版本发布成功，当前版本号v$version#*//----" "[32m"
-        print "----请使用 npm i @baidu/med-ui@$version#*// -S --registry=http://registry.npm.baidu-int.com 更新依赖----" "[32m"
+        print "----版本发布成功，当前版本号v$version----" "[32m"
+        print "----请使用 npm i @baidu/med-ui@$version -S --registry=http://registry.npm.baidu-int.com 更新依赖----" "[32m"
         print "----请自行确认本次更改的代码，是否要推送到远程仓库（git push origin HEAD:refs/for/master）----" 
         exit 0
     else
